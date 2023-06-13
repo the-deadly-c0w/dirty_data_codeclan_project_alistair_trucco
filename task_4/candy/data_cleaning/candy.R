b42b5cab505671206948add0829cfb6fcede1c9d
the-deadly-c0w <masdofswords@gmail.com>
2023-06-13 11:17:28 +0100

    ages, started country

--- task_4/candy/data_cleaning/candy.R
+++ task_4/candy/data_cleaning/candy.R
@@ -53,9 +53,99 @@ rename("age" = "how_old_are_you",
 
 
 
+#                     COUNTRY
 
 
 
+
+# finding all spellings of all countries to decide how to standardise them
+
+
+unique(candy_2016_renamed$country)
+#93
+# holy shit
+
+unique(candy_2017_chopped$country)
+# 118
+
+# h o l y s h i t
+# it's a lucky thing for the participants that 
+# they didn't include address or emails in this survey 
+
+# start by changing everything to lower case, should rid us of a lot of overlap
+
+candy_2016_renamed <- candy_2016_renamed %>% 
+  mutate(country_clean = str_to_lower(country))
+
+candy_2017_chopped <- candy_2017_chopped %>% 
+  mutate(country_clean = str_to_lower(country))
+
+
+unique(candy_2016_renamed$country_clean)
+#75
+
+
+unique(candy_2017_chopped$country_clean)
+# 96
+
+
+# testing if only usa entries have the word state
+
+test <- candy_2016_renamed %>% 
+  filter(str_detect(country_clean, "state")) %>% 
+  select(country_clean)
+unique(test)
+
+
+
+candy_2016_renamed <- candy_2016_renamed %>% 
+  mutate(country_clean = 
+           if(str_detect(country_clean, "state")){
+            "usa"
+})
+
+
+for(i in 1:length(candy_2016_renamed$country_clean)){
+  if(str_detect(candy_2016_renamed$country_clean[i], "state")){
+    candy_2016_renamed$country_clean[i] = "usa"
+}
+}
 #setdiff(colnames(candy_2015_chopped), colnames(candy_2016_chopped)) 
 #setdiff(colnames(candy_2016_chopped), colnames(candy_2015_chopped))
 #setdiff(colnames(candy_2016_chopped), colnames(candy_2017_chopped)) 
+
+
+test <- candy_2016_renamed %>% 
+  filter(str_detect(country_clean, "state")) %>% 
+  select(country_clean)
+unique(test)
+
+
+
+
+
+
+#                          AGE
+
+
+# changing the 
+
+candy_2015_age <- candy_2015_renamed %>% 
+  mutate(age = as.integer(age))
+
+
+candy_2016_age <- candy_2016_renamed %>% 
+  mutate(age = as.integer(age))
+
+
+#candy_2015_age %>% 
+#  filter(is.na(age)) %>% 
+#  nrow()
+#285 compared with 200
+
+#candy_2016_renamed %>% 
+#  filter(is.na(age)) %>% 
+#  nrow()
+#68 compared with 68
+
+
